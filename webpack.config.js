de32785ba28b4e5be17ba0dfc52120c4af6ca45d
David <david.waller@svenskaspel.se>
2018-10-05 10:26:32 +0200

    first rough test setup

--- webpack.config.js
+++ webpack.config.js
@@ -45,39 +45,3 @@ module.exports = {
   },
   devtool: 'inline-source-map'
 };
-
-
-/*
-let webpack = require('webpack');
-let path = require('path');
-
-module.exports = {
-  devtool: 'eval',
-  entry: __dirname + '/main.ts',
-  output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js',
-    sourceMapFilename: 'source.map'
-  },
-  resolve: {
-    extensions: [".ts",".js"]
-  },
-  module: {
-    rules: [{
-      test: /\.ts$/,
-      loader: 'ts-loader',
-      exclude: ['node_modules']
-    }]
-  },
-  
-  //  Hack to avoid build warnings about "the request of a dependency is an expression"
-  // See https://github.com/angular/angular/issues/11580
-  plugins: [
-    new webpack.ContextReplacementPlugin(
-      /angular(\\|\/)core(\\|\/)@angular/,
-      path.resolve('./src'),
-      {}
-    )
-  ],
-}
-*/
